b4d030614b8fbc12f210265baa44525904047e04
Artur Dymkowski <arturdymkowski@wp.pl>
2019-11-23 10:42:17 +0100

    DOdanie serializerów

--- SprzedazKsiazek/serializers.py
+++ SprzedazKsiazek/serializers.py
@@ -5,3 +5,27 @@ class kategoriaSerializer(serializers.ModelSerializer):
     class Meta:
         model = kategoria
         fields = ['idKategoria','nazwa']
+class autorSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = autor
+        fields = ['idAutor','imie','nazwisko']
+
+class ksiazkaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = ksiazka
+        fields = ['idKsiazka','nazwa','rokWydania','cena','idAutor','idKategoria']
+
+class klientSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = klient
+        fields = ['idKlient','imie','nazwisko','pesel','nrKontaktowy']
+
+class pracownikSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = pracownik
+        fields = ['idPracownik','imie','nazwisko','pesel','nrKontaktowy','zarobki']
+
+class zamowieniaSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = zamowienia
+        fields = ['idZamowienia','idKsiazka','idKlient','idPracownik','cena']
