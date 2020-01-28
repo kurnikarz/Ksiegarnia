088917bdc53a34ab007cd59ee25ddc00a7128267
Artur Dymkowski <arturdymkowski@wp.pl>
2020-01-28 23:01:38 +0100

    Widoczki

--- SprzedazKsiazek/views.py
+++ SprzedazKsiazek/views.py
@@ -65,7 +65,7 @@ class pracownik_list(APIView):
     def post(self, request, format=None):
         serializer = pracownikSerializer(data=request.data)
         if serializer.is_valid():
-
+            serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
